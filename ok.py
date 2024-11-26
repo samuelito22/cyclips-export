939c6055260853f4df644bdff48e75f43476070f
Samuel <sedorodion@hotmail.com>
2024-11-26 15:09:46 +0000

    ok

--- ok.py
+++
@@ -1,3 +0,0 @@
-from src.export import Exporter
-
-Exporter().export(video_path="low_res_video(1).mp4", start=10, end=50, scenes_path="cropping_boxes(1).json", output_path="ok3.mp4", subtitles_path="subtitles copy.ass")
